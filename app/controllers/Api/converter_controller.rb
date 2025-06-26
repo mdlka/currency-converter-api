3bef5c3c7002ea03457288f41b253ddef10255e0
mdlka <mogilevskiy499@gmail.com>
2025-06-26 15:09:38 +0300

    Improve convert response

--- app/controllers/Api/converter_controller.rb
+++ app/controllers/Api/converter_controller.rb
@@ -1,6 +1,12 @@
 class Api::ConverterController < ApplicationController
   def convert
-    render json: ConverterService.new(from: params[:from], to: params[:to], amount: params[:amount])
-                                 .convert
+    convert_result = ConverterService.new(from: params[:from], to: params[:to], amount: params[:amount]).convert
+    conversion = Conversion.new(from: params[:from], to: params[:to], amount: params[:amount], result: convert_result)
+
+    if conversion.save
+      render json: conversion
+    else
+      render json: { errors: conversion.errors.full_messages }, status: :unprocessable_content
+    end
   end
 end
